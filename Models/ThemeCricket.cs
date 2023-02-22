05317ab200496c090b940c4350182825878fafde
Arya0104 <Arya0104@users.noreply.github.com>
2023-02-21 22:23:13 -0500

    Model Folder and file created as ThemeCricket.cs

---
+++ Models/ThemeCricket.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEB315_Assignments_ThemeCricket.Models
+{
+    public class ThemeCricket
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
